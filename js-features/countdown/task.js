faf841d293eb4e8b705bbf2491190911c81015b3
DrAVl <DrAV432@mail.ru>
2024-03-25 12:05:23 +0700

    задание таймер готово

---
+++ js-features/countdown/task.js
@@ -0,0 +1,14 @@
+const timerCount = document.getElementById("timer");
+const maxValue = timerCount.textContent;
+let timerValue = new Date(0, 0, 0, 0, 0, maxValue);
+
+let countdown = function() {
+  timerValue.setTime(timerValue.getTime() - 1000);
+  timerCount.textContent = timerValue.toLocaleTimeString("ru-Ru", { hour: "2-digit", minute: "2-digit", second: "2-digit" });
+  if (timerValue.getSeconds() === 0) {
+    alert('Время вышло!');
+  } 
+} 
+  
+let timerId = setInterval(countdown, 1000);
+setTimeout(() => clearInterval(timerId), maxValue * 1000);
